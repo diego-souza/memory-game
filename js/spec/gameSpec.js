c9719f36e27054fbe7512d7f3d31ca5405f796a1
Diego Alves de Souza <diegosouza.mail@gmail.com>
2013-01-02 17:20:37 -0200

    Game has a board with pieces.

--- js/spec/gameSpec.js
+++ js/spec/gameSpec.js
@@ -2,16 +2,30 @@ describe("Game", function() {
 
   describe("New Game", function(){
     it("should be able to start a game", function() {
-        expect(new Game()).toBeDefined;
-      });
+      expect(new MemoryGame.Game()).toBeDefined;
+    });
 
     it("should have 5 pairs by default", function() {
-        expect(new Game().remainingPairs).toEqual(5);
-      });
+      expect(new MemoryGame.Game().remainingPairs()).toEqual(5);
+    });
 
     it("should be able to start game with number of pairs", function() {
-        expect(new Game(6).remainingPairs).toEqual(6);
-      });
-  })
+      expect(new MemoryGame.Game(6).remainingPairs()).toEqual(6);
+    });
+  });
+
+  describe("board", function(){
+
+    it("should have a board", function() {
+      expect(new MemoryGame.Game().board()).toBeDefined;
+    });
+
+    it("should have pieces on the board", function() {
+      game = new MemoryGame.Game(2)
+      expect(game.board().length).toEqual(2);
+      expect(game.board()[0] instanceof MemoryGame.Piece).toBeTruthy()
+    });
+
+  });
 
 });
--- js/src/game.js
+++ js/src/game.js
@@ -1,5 +1,33 @@
-function Game(numberOfPairs){
-  numberOfPairs = typeof numberOfPairs !== 'undefined' ? numberOfPairs : 5;
-  this.remainingPairs = numberOfPairs;
+var MemoryGame = MemoryGame || {}
+
+MemoryGame.Game = function (numberOfPairs){
+  var self = this
+  self.pairs = typeof numberOfPairs !== 'undefined' ? numberOfPairs : 5;
+  self.gameBoard = new MemoryGame.Board
+
+  self.remainingPairs = function (){
+    return self.pairs
+  }
+
+  self.board = function (){
+    return self.gameBoard.space()
+  }
+
+  self.play = function(pieceNumber){
+
+  }
 }
 
+MemoryGame.Board = function(){
+  var self = this
+  self.space = function(){
+    return [new MemoryGame.Piece, new MemoryGame.Piece]
+  }
+
+  self.play = function(){
+  }
+}
+
+MemoryGame.Piece = function(){
+  var self = this
+}
